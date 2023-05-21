8c5c8d18ad75a47f5a980a635c993575963c73a4
Dhanushki Mapitigama <dhanumapitigama@gmail.com>
2023-05-21 20:48:02 +0200

    fixed prev commit

--- parallelization/galsim.c
+++ parallelization/galsim.c
@@ -346,8 +346,10 @@ void *update_acceleration_v2(void *arg)
     //printf("Velocity-tmp-x after loop: %lf, %d\n", tmp_velx[3],  thread_input->start_n);
 
     for (int m = 0; m < thread_input->N; m++){
+        pthread_mutex_lock(&mutex);
         thread_input->particles->velx[m] += tmp_velx[m];
         thread_input->particles->vely[m] += tmp_vely[m];
+        pthread_mutex_unlock(&mutex);
     }
 
     //printf("Velocity--x after update: %lf, %d\n", thread_input->particles->velx[3],  thread_input->start_n);
@@ -378,10 +380,8 @@ void *update_position_v2(void *arg)
 
     for (int i = thread_input->start_n; i < thread_input->end_n; i++)
     {
-        pthread_mutex_lock(&mutex);
         thread_input->particles->posx[i] += thread_input->particles->velx[i] * thread_input->delta_t;
         thread_input->particles->posy[i] += thread_input->particles->vely[i] * thread_input->delta_t;
-        pthread_mutex_unlock(&mutex);
     }
 
     return NULL;
